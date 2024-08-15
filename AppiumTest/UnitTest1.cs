1f55b2791c10a6b8e12b4f6a1ab71b85964bb574
Lennart Kroneman <lennart.kroneman@daveramsey.com>
2024-08-15 14:17:36 -0500

    initial project setup

---
+++ AppiumTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AppiumTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ SyncFusionListItemViewRecycling/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SyncFusionListItemViewRecycling;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
